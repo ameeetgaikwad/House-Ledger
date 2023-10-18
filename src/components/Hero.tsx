2b9f6a1a0254bde58b0a7e757be88a45150bc1f2
Rahul_Singh <koderrahul@gmail.com>
2023-10-18 11:51:11 +0530

    feat: basic hero section

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,7 +1,16 @@
 import React from "react";
 
 const Hero = () => {
-  return <div>h</div>;
+  return (
+    <div className=" mt-16 h-full w-full p-5 m-5 bg-[url('/images/wepik-export-20231018060249SBSG.png')] bg-[position:center_center] bg-[length:auto] bg-no-repeat">
+      <div className="text-white">
+        <p className=" mb-10 font-semibold text-5xl ml-10">
+          Find Your Best
+          <span className="text-[#1252AE] font-bold "> Real Estate </span>
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Hero;
