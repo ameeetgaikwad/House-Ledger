be3fe3cdd9adf45a37ef7c59b81b0cf8669f8043
Rahul_Singh <koderrahul@gmail.com>
2023-10-18 12:01:12 +0530

    fix: text size

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -4,9 +4,11 @@ const Hero = () => {
   return (
     <div className=" mt-16 h-full w-full p-5 m-5 bg-[url('/images/wepik-export-20231018060249SBSG.png')] bg-[position:center_center] bg-[length:auto] bg-no-repeat">
       <div className="text-white">
-        <p className=" mb-10 font-semibold text-5xl ml-10">
-          Find Your Best
-          <span className="text-[#1252AE] font-bold "> Real Estate </span>
+        <p className=" mb-10 font-semibold text-4xl ml-10">
+          Find Your Best{" "}
+          <span className="text-blue-700 underline font-bold text-5xl">
+            Real Estate{" "}
+          </span>
         </p>
       </div>
     </div>
