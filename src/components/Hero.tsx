54ba1df329839f2507f65c13120fbbaabc72f434
Rahul_Singh <koderrahul@gmail.com>
2023-10-19 21:48:58 +0530

    fix:space in hero

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Hero = () => {
   return (
-    <div className=" mt-16 h-full w-full p-5 m-5 bg-[url('/images/wepik-export-20231018060249SBSG.png')] bg-[position:center_center] bg-[length:auto] bg-no-repeat">
+    <div className=" mt-16 h-full w-full p-5  bg-[url('/images/wepik-export-20231018060249SBSG.png')] bg-[position:center_center] bg-[length:auto] bg-no-repeat">
       <div className="text-white">
         <p className=" mb-10 font-semibold text-4xl ml-10">
           Find Your Best{" "}
